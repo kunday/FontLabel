43076b4f6967bd4c69d7ea8c14b78d24eecf1a3e
Arvind Kunday <arvinda@thoughtworks.com>
2011-04-25 18:10:12 +0530

    Adding strikethrough functionality to font label . we are using the font height diving it by 2 and drawing line in that position

--- FontLabel/Classes/FontLabel/FontLabelStringDrawing.m
+++ FontLabel/Classes/FontLabel/FontLabelStringDrawing.m
@@ -343,6 +343,7 @@ static id getValueOrDefaultForRun(ZAttributeRun *run, NSString *key) {
 							 [UIColor blackColor], ZForegroundColorAttributeName,
 							 [UIColor clearColor], ZBackgroundColorAttributeName,
 							 [NSNumber numberWithInt:ZUnderlineStyleNone], ZUnderlineStyleAttributeName,
+                             [NSNumber numberWithInt:ZUnderlineStyleNone],  ZStrikeThroughStyleAttributeName,
 							 nil];
 		}
 		value = [defaultValues objectForKey:key];
@@ -694,6 +695,11 @@ static CGSize drawOrSizeTextConstrainedToSize(BOOL performDraw, NSString *string
 							// we only support single for the time being
 							UIRectFill(CGRectMake(drawPoint.x, drawPoint.y + lineAscender, fragmentWidth, 1));
 						}
+                        NSNumber *strikeThroughStyle = getValueOrDefaultForRun(currentRun, ZStrikeThroughStyleAttributeName);
+                        if([strikeThroughStyle integerValue]) {
+                            UIRectFill(CGRectMake(drawPoint.x, drawPoint.y - currentFont.xHeight / 2 + lineAscender, fragmentWidth, 2));
+						}
+
 						drawPoint.x += fragmentWidth;
 						glyphIdx += numGlyphs;
 					}
